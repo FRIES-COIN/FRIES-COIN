ed5790c3833c85c2efcdde6d06fe31c5248f4443
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-05-24 10:06:05 +0300

    Initial Setup

--- src/FRIES-COIN-frontend/src/App.jsx
+++ src/FRIES-COIN-frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { FRIES_COIN_backend } from 'declarations/FRIES-COIN-backend';
 
 function App() {
   const [greeting, setGreeting] = useState('');
-
+ 
   function handleSubmit(event) {
     event.preventDefault();
     const name = event.target.elements.name.value;
@@ -15,13 +15,16 @@ function App() {
 
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
+      <h1 className="text-center my-8 text-red-500 text-xl font-bold">FRIES COIN Starter</h1>
+     <div className="flex items-center justify-center">
+       <img src="/logo2.svg" alt="DFINITY logo" />
+     </div> 
       <br />
       <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
+      <form action="#" onSubmit={handleSubmit} className="flex flex-col max-w-[400px] gap-4 mx-auto">
+        <label htmlFor="name" className="text-center">Enter your name: &nbsp;</label>
+        <input id="name" alt="Name" type="text" className="border-1 p-4 rounded bg-gray-200 mx-4 text-black font-semibold" />
+        <button type="submit" className="bg-orange-500 text-white p-4 rounded shadow-md ">Click Me!</button>
       </form>
       <section id="greeting">{greeting}</section>
     </main>
