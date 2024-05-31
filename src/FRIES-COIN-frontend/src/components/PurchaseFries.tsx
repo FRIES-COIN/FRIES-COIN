b4b571ba28a8e184a5ae3f623875bea9b214d6ef
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-05-31 09:20:38 +0300

    Refactor App component to fix typo and improve code formatting

--- src/FRIES-COIN-frontend/src/components/PurchaseFries.tsx
+++ src/FRIES-COIN-frontend/src/components/PurchaseFries.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 interface PurchaseFriesProps {
   onClose: () => void;
 }
 
 const PurchaseFriesPopup: React.FC<PurchaseFriesProps> = ({ onClose }) => {
-  const [amount, setAmount] = useState('');
+  const [amount, setAmount] = useState("");
 
   const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setAmount(e.target.value);
@@ -18,16 +18,22 @@ const PurchaseFriesPopup: React.FC<PurchaseFriesProps> = ({ onClose }) => {
 
   return (
     <div
-      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
+      // className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
+      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 backdrop-blur"
       onClick={onClose}
     >
       <div
-        className="bg-white p-8 rounded-lg z-10 w-96"
+        className="bg-[#041c32] p-8 rounded-lg z-10 w-96"
         onClick={(e) => e.stopPropagation()}
       >
-        <h2 className="text-xl font-bold mb-4 text-center">Purchase $FRIES</h2>
+        <h2 className="text-xl font-bold mb-4 text-center text-gray-300 font-rem">
+          Purchase $FRYS
+        </h2>
         <div className="flex flex-col items-center mb-4">
-          <label htmlFor="amount" className="block mb-2 font-bold">
+          <label
+            htmlFor="amount"
+            className="block mb-2 font-bold text-gray-300 font-rem"
+          >
             Amount
           </label>
           <input
@@ -35,22 +41,22 @@ const PurchaseFriesPopup: React.FC<PurchaseFriesProps> = ({ onClose }) => {
             id="amount"
             value={amount}
             onChange={handleAmountChange}
-            className="w-full px-4 py-2 border border-gray-300 rounded"
+            className="w-full px-4 font-rem py-2 border border-gray-300 rounded bg-gray-800 text-gray-300 focus:outline-none focus:border-[#ffd543] transition-all duration-300"
             placeholder="Enter amount"
           />
         </div>
         <div className="flex justify-center mb-4">
           <button
-            className="px-4 py-2 bg-blue-500 text-white rounded transition-all duration-300 hover:bg-blue-700 hover:scale-105 hover:shadow-lg"
+            className="px-4 py-2 font-rem bg-[#ffd543] text-[#041c32] border-2 border-[#ffd543] rounded-lg"
             onClick={handlePurchase}
             disabled={!amount}
           >
-            Purchase $FRIES
+            Purchase $FRYS
           </button>
         </div>
         <div className="flex justify-center">
           <button
-            className="mt-4 px-4 py-2 bg-gray-200 rounded"
+            className="mt-4 px-4 py-2 bg-gray-200 rounded font-rem"
             onClick={onClose}
           >
             Close
