9d44cd8faa4201d6c0e1d1090cc7d94593118622
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-05-31 08:24:24 +0300

    Refactor About component to update vision statement and remove unnecessary link

--- src/FRIES-COIN-frontend/src/components/WalletPopup.tsx
+++ src/FRIES-COIN-frontend/src/components/WalletPopup.tsx
@@ -61,17 +61,20 @@ const WalletPopup: React.FC<WalletPopupProps> = ({
 
   return (
     <div
-      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
+      // className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
+      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 backdrop-blur"
       onClick={onClose}
     >
       <div
-        className="bg-white p-8 rounded-lg z-10 w-96"
+        className="bg-[#041c32] p-8 rounded-lg z-10 w-96"
         onClick={(e) => e.stopPropagation()}
       >
-        <h2 className="text-xl font-bold mb-4 text-center">Choose a Wallet</h2>
+        <h2 className="text-xl text-gray-300 font-bold mb-4 text-center">
+          Choose a Wallet
+        </h2>
         <div className="flex justify-between mb-4">
           <button
-            className={`px-4 py-2 bg-blue-500 text-white rounded transition-all duration-300 hover:bg-blue-700 hover:scale-105 hover:shadow-lg ${
+            className={`bg-[#ffd543] text-[#041c32] font-semibold font-rem hover:text-white py-2 px-4 rounded-lg hover:bg-transparent border-2 border-[#ffd543] duration-300 transition-all  hover:scale-105 hover:shadow-lg ${
               isConnecting ? "opacity-50 cursor-not-allowed" : ""
             }`}
             onClick={connectPlugWallet}
@@ -79,7 +82,7 @@ const WalletPopup: React.FC<WalletPopupProps> = ({
           >
             {isConnecting ? "Connecting..." : "Plug Wallet"}
           </button>
-          <button className="px-4 py-2 bg-blue-500 text-white rounded transition-all duration-300 hover:bg-blue-700 hover:scale-105 hover:shadow-lg">
+          <button className="bg-[#ffd543] text-[#041c32] font-semibold text-lg font-rem md:text-xl hover:text-white py-2 px-4 rounded-lg hover:bg-transparent border-2 border-[#ffd543] transition duration-300">
             NFiD
           </button>
         </div>
@@ -103,7 +106,7 @@ const WalletPopup: React.FC<WalletPopupProps> = ({
         )}
         <div className="flex justify-center">
           <button
-            className="mt-4 px-4 py-2 bg-gray-200 rounded"
+            className="mt-4 px-4 py-2 bg-gray-300 rounded"
             onClick={onClose}
           >
             Close
